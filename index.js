fad5cb94b7143f4b9d8bd8976a4226d70dcc3f60
sorry510 <491559675@qq.com>
2021-10-16 14:11:34 +0800

    fix: 修改交易间隔时间

--- index.js
+++ index.js
@@ -228,7 +228,7 @@ async function init() {
       log('wait 120 seconds')
       await sleep(120 * 1000) // 有交易成功的时候，暂停交易 2 min
     } else {
-      await sleep(config.sleep_time || 60 * 1000) // 无交易时，暂停 sleep_time 秒
+      await sleep((config.sleep_time || 60) * 1000) // 无交易时，暂停 sleep_time 秒
     }
   }
 })()
