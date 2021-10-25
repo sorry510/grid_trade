411abb048a2bceba535e3edb3e1d0a6f1daf1963
sorry510 <491559675@qq.com>
2021-10-25 14:10:06 +0800

    fix: 修改登陆时效&前端界面

--- web/back/app.js
+++ web/back/app.js
@@ -74,7 +74,7 @@ app.post('/login', (req, res) => {
         },
         secret,
         {
-          expiresIn: 3600 * 24 * 1, // 过期时间
+          expiresIn: 3600 * 24 * 7, // 过期时间
         }
       )
     res.json(
@@ -131,13 +131,15 @@ app.put('/trades', (req, res) => {
   const oldTradeList = JSON.parse(tradeJson)
 
   let result = true
-  if (trades.length != oldTradeList.length) {
-    result = false
-    res.json(resJson(202, '交易类型数量不一致, 请刷新数据进行同步'))
-  }
+  // if (trades.length != oldTradeList.length) {
+  //   result = false
+  //   res.json(resJson(202, '交易类型数量不一致, 请刷新数据进行同步'))
+  // }
+
   if (result) {
     for (let i = 0; i < oldTradeList.length; i++) {
-      if (oldTradeList[i].history_trade.length !== trades[i].history_trade.length) {
+      const find = trades.find((item) => item.symbol === oldTradeList[i].symbol)
+      if (find && oldTradeList[i].history_trade.length !== find.history_trade.length) {
         result = false
         res.json(resJson(203, '交易历史记录不一致, 请刷新数据进行同步'))
         break
