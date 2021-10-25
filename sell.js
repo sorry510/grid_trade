a52de3557b4addea7bc26bb88c3fa67494b9cd35
sorry510 <491559675@qq.com>
2021-10-25 17:32:53 +0800

    fix: 修改代码错误

--- sell.js
+++ sell.js
@@ -38,85 +38,88 @@ async function init() {
           return trade
         }
 
+        const nowPrice = await Api.getTickerPrice(symbol) // 最新价格
+
         const sell_history_trade = [] // 要进行的卖单操作记录
 
         // 遍历买单记录
-        const new_history_trade = history_trade.map((item) => {
-          if (item.side === BuySide.SELL) {
-            return item // 卖单记录
-          }
-          if (item.side === BuySide.BUY && item.isSell === true) {
-            return item // 已经卖出的买单记录
-          }
-          if (nowPrice < item.lowest_sell_price) {
-            return item // 低于最低的卖出价格
-          }
-          if (nowPrice >= item.sell_price) {
-            // 当前价格>预定卖出价格
-            item.sell_price = nowPrice // 将最新价格定为卖出价格
-            item.low_num = 0
-            return item
-          }
-          const midPrice =
-            (item.sell_price - item.lowest_sell_price) * 0.55 + item.lowest_sell_price // 中间价格
-          if (nowPrice >= midPrice) {
-            item.low_num = 0
-            return item
-          }
-          if (item.low_num < 2) {
-            item.low_num += 1 // 添加容错，第3次触发条件，才进行卖出操作
-            return item
-          }
-
-          // 价格回落到 (最高价格-最初卖价格) / 50% + 最初卖价格，通过容错机制，进行卖出操作
-          let quantityTrue = item.quantity // 交易单的买入数量
-          if (buy_quantity < quantityTrue) {
-            quantityTrue = buy_quantity // 当前拥有的数量为最大可交易数量
-          }
-          let res
-          if (quantityTrue > 0) {
-            try {
-              res = await Api.order(symbol, BuySide.SELL, OrderType.LIMIT, {
-                timeInForce: TimeInForce.GTC, // 成交为止，订单会一直有效
-                quantity: quantityTrue, // 交易数量
-                price: canTradePrice(nowPrice), // marker 模式一直报错，只能使用limit,每个币的小数位数不同，需要截取不同位数
-              }) // 以当前市价下单
-            } catch (e) {
-              // 当前撮合交易失败
-              log(e)
-              Api.notifySellOrderFail(symbol, e)
+        const new_history_trade = await Promise.all(
+          history_trade.map(async (item) => {
+            if (item.side === BuySide.SELL) {
+              return item // 卖单记录
+            }
+            if (item.side === BuySide.BUY && item.isSell === true) {
+              return item // 已经卖出的买单记录
+            }
+            if (nowPrice < item.lowest_sell_price) {
+              return item // 低于最低的卖出价格
+            }
+            if (nowPrice >= item.sell_price) {
+              // 当前价格>预定卖出价格
+              item.sell_price = nowPrice // 将最新价格定为卖出价格
+              item.low_num = 0
               return item
             }
-            if (res && res.orderId) {
-              item.isSell = true // 更新此记录为已卖出
-
-              let tradePrice = nowPrice
-              if (res['fills'] && res['fills'][0] && res['fills'][0]['price']) {
-                tradePrice = res['fills'][0]['price'] // 交易价格
-              }
-              log(
-                `币种为：${symbol}, 卖单量为：${quantityTrue}, 卖单价格为：${tradePrice}。预计盈利: ${profit} USDT`
-              )
-              Api.notifySellOrderSuccess(symbol, quantityTrue, tradePrice, profit) // 发送通知
-              // 卖单记录
-              sell_history_trade.unshift({
-                symbol, // 币种
-                quantity: quantityTrue, // 交易数量
-                price: tradePrice, // 卖单价格
-                side: BuySide.SELL, // 方向 卖
-                buy_price: item.price, // 对应的买入价格
-                profit: (tradePrice - item.price) * quantityTrue, // 盈利多少
-                time: dateFormat(), // 时间
-              }) // 像头部插入一条，这样容易直接找到最新的记录
-
-              trade.buy_quantity -= quantityTrue // 更新已购买数量
-              trade.buy_price = round(tradePrice * (1 - trade.rate / 100), 6) // 更新买入价格
-              trade.sell_price = round(tradePrice * (1 + trade.rate / 100), 6) // 更新的卖出价格
+            const midPrice =
+              (item.sell_price - item.lowest_sell_price) * 0.55 + item.lowest_sell_price // 中间价格
+            if (nowPrice >= midPrice) {
+              item.low_num = 0
+              return item
             }
-          }
+            if (item.low_num < 2) {
+              item.low_num += 1 // 添加容错，第3次触发条件，才进行卖出操作
+              return item
+            }
 
-          return item
-        })
+            // 价格回落到 (最高价格-最初卖价格) / 50% + 最初卖价格，通过容错机制，进行卖出操作
+            let quantityTrue = item.quantity // 交易单的买入数量
+            if (buy_quantity < quantityTrue) {
+              quantityTrue = buy_quantity // 当前拥有的数量为最大可交易数量
+            }
+            let res
+            if (quantityTrue > 0) {
+              try {
+                res = await Api.order(symbol, BuySide.SELL, OrderType.LIMIT, {
+                  timeInForce: TimeInForce.GTC, // 成交为止，订单会一直有效
+                  quantity: quantityTrue, // 交易数量
+                  price: canTradePrice(nowPrice), // marker 模式一直报错，只能使用limit,每个币的小数位数不同，需要截取不同位数
+                }) // 以当前市价下单
+              } catch (e) {
+                // 当前撮合交易失败
+                log(e)
+                Api.notifySellOrderFail(symbol, e)
+                return item
+              }
+              if (res && res.orderId) {
+                item.isSell = true // 更新此记录为已卖出
+
+                let tradePrice = nowPrice
+                if (res['fills'] && res['fills'][0] && res['fills'][0]['price']) {
+                  tradePrice = res['fills'][0]['price'] // 交易价格
+                }
+                log(
+                  `币种为：${symbol}, 卖单量为：${quantityTrue}, 卖单价格为：${tradePrice}。预计盈利: ${profit} USDT`
+                )
+                Api.notifySellOrderSuccess(symbol, quantityTrue, tradePrice, profit) // 发送通知
+                // 卖单记录
+                sell_history_trade.unshift({
+                  symbol, // 币种
+                  quantity: quantityTrue, // 交易数量
+                  price: tradePrice, // 卖单价格
+                  side: BuySide.SELL, // 方向 卖
+                  buy_price: item.price, // 对应的买入价格
+                  profit: (tradePrice - item.price) * quantityTrue, // 盈利多少
+                  time: dateFormat(), // 时间
+                }) // 像头部插入一条，这样容易直接找到最新的记录
+
+                trade.buy_quantity -= quantityTrue // 更新已购买数量
+                trade.buy_price = round(tradePrice * (1 - trade.rate / 100), 6) // 更新买入价格
+                trade.sell_price = round(tradePrice * (1 + trade.rate / 100), 6) // 更新的卖出价格
+              }
+            }
+            return item
+          })
+        )
         trade.history_trade = [...sell_history_trade, ...new_history_trade] // 记录历史记录
         return trade
       })
