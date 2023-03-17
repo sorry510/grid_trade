75d917e83f8adb0a4ae606172edf9b7c4cdbfd22
HaoZhengZhao <haozheng.zhao@poper.co>
2023-03-17 08:42:30 +0000

    fix: 修复bug

--- quick.js
+++ quick.js
@@ -44,107 +44,19 @@ async function init() {
           buy_open, // 买单开启
           sell_open, // 卖单开启
           stop_loss = 0,
-          history_trade = [],
         } = trade
+        // 没有开启买卖
         if (!buy_open && !sell_open) {
-          // 没有开启买卖
           return trade
         }
 
         const nowPrice = await Api.getTickerPrice(symbol) // 最新价格
-        const [k1, k2] = await Api.getMaCompare(symbol, KlineType['1m'], [3, 30])  // 1min 的 kline 最近 3 条 与 最近 30 条
-        if (k1 > k2) {
-          // 涨的趋势，买
-          if (buy_quantity > 0) {
-            // 已有买单，不再买了
-            return trade
-          } else {
-            // 下单金额过小
-            if (quantity * nowPrice < 10) {
-              const msg = `币种${symbol} 的下单金额必须 >= 10 usdt，已关闭买单操作`
-              log(msg, flag)
-              Api.notifyServiceError(msg)
-              trade.buy_open = false
-              return trade
-            }
-            // 判定是否下单
-            let res
-            try {
-              res = await Api.order(symbol, BuySide.BUY, OrderType.LIMIT, {
-                timeInForce: TimeInForce.GTC, // 成交为止，订单会一直有效
-                quantity, // 交易数量
-                price: canTradePrice(nowPrice),
-              }) // 以当前价格下单
-              // test
-              // res = {
-              //   orderId: 1,
-              //   fills: [{ price: 423 }],
-              // }
-            } catch (e) {
-              // 当前撮合交易失败
-              let myUsdt = 0
-              try {
-                myUsdt = await Api.getWalletUsdt() // 账户中的 usdt 数量
-              } catch (e) {}
-              if (nowPrice * quantity > myUsdt) {
-                Api.notifyBuyOrderFail(symbol, '当前账户余额已不足,已关闭买单操作')
-                trade.buy_open = false
-                return trade
-              } else {
-                if (!trade.error_num) {
-                  trade.error_num = 1
-                } else {
-                  trade.error_num++
-                  if (trade.error_num > 3) {
-                    // 错误次数大于3，停止当前币种交易
-                    trade.buy_open = false
-                  }
-                }
-                Api.notifyBuyOrderFail(symbol, e)
-              }
-              return trade
-            }
-            if (res && res.orderId) {
-              // 交易成功
-              result = true
-              let tradePrice = nowPrice // 首先默认交易价格为当前价格
-              if (res['fills'] && res['fills'].length > 0 && res['fills'][0]['price']) {
-                tradePrice = res['fills'][0]['price'] // 更新为交易记录中的第一个价格
-              }
-              log(`买入币种为：${symbol}, 买单量为：${quantity}, 买单价格为：${tradePrice}`, flag)
-              Api.notifyBuyOrderSuccess(symbol, quantity, tradePrice) // 发送通知
 
-              trade.buy_price = tradePrice // 最后一次买入价格
-              trade.sell_price = round(tradePrice * (1 + trade.rate / 100), 6) // 更新的卖出价格
-              if (trade.sell_price < nowPrice) {
-                trade.sell_price = round(nowPrice * (1 + trade.rate / 100), 6) // 更新的卖出价格
-              }
-
-              // 买单记录
-              history_trade.unshift({
-                symbol,
-                quantity,
-                price: tradePrice,
-                side: BuySide.BUY,
-                time: dateFormat(),
-                sell_price: trade.sell_price, // 根据当时的买入价格，设定应该卖出的价格
-                lowest_sell_price: trade.sell_price, // 最低卖价
-                low_num: 0, // 容错次数
-                stop_loss: stop_loss, // 止损率
-                isSell: false,
-              }) // 头部插入一条，这样容易直接找到最新的记录
-              trade.history_trade = history_trade // 更新买卖历史记录
-              log(trade, flag)
-            }
-          }
-        } else {
-          // 跌的趋势, 卖
-          if (buy_quantity <= 0) {
-            return trade
-          }
+        // 查看是否达到盈利点可以卖出
+        if (sell_open && buy_quantity > 0) {
           const sell_history_trade = [] // 要进行的卖单操作记录
           const new_history_trade = await Promise.all(
-            history_trade.map(async (item) => {
+            trade.history_trade.map(async (item) => {
               // 卖单记录
               if (item.side === BuySide.SELL) {
                 return item
@@ -207,6 +119,96 @@ async function init() {
             0
           ) // 更新当前购买数量
           trade.history_trade = [...sell_history_trade, ...new_history_trade] // 记录历史记录
+        }
+
+        // 检查是否要买入
+        if (buy_open && buy_quantity == 0) {
+          const [m1, m2, m3] = await Api.getMaCompare(symbol, KlineType['1m'], [3, 3 * 10, 3 * 20]) // 1min的kline
+          if (m1 > m2 && m2 > m3) {
+            // 涨的趋势，买
+            if (buy_quantity > 0) {
+              // 已有买单，不再买了
+              return trade
+            } else {
+              // 下单金额过小
+              if (quantity * nowPrice < 10) {
+                const msg = `币种${symbol} 的下单金额必须 >= 10 usdt，已关闭买单操作`
+                log(msg, flag)
+                Api.notifyServiceError(msg)
+                trade.buy_open = false
+                return trade
+              }
+              // 判定是否下单
+              let res
+              try {
+                res = await Api.order(symbol, BuySide.BUY, OrderType.LIMIT, {
+                  timeInForce: TimeInForce.GTC, // 成交为止，订单会一直有效
+                  quantity, // 交易数量
+                  price: canTradePrice(nowPrice),
+                }) // 以当前价格下单
+                // test
+                // res = {
+                //   orderId: 1,
+                //   fills: [{ price: 423 }],
+                // }
+              } catch (e) {
+                // 当前撮合交易失败
+                let myUsdt = 0
+                try {
+                  myUsdt = await Api.getWalletUsdt() // 账户中的 usdt 数量
+                } catch (e) {}
+                if (nowPrice * quantity > myUsdt) {
+                  Api.notifyBuyOrderFail(symbol, '当前账户余额已不足,已关闭买单操作')
+                  trade.buy_open = false
+                  return trade
+                } else {
+                  if (!trade.error_num) {
+                    trade.error_num = 1
+                  } else {
+                    trade.error_num++
+                    if (trade.error_num > 3) {
+                      // 错误次数大于3，停止当前币种交易
+                      trade.buy_open = false
+                    }
+                  }
+                  Api.notifyBuyOrderFail(symbol, e)
+                }
+                return trade
+              }
+              if (res && res.orderId) {
+                // 交易成功
+                result = true
+                let tradePrice = nowPrice // 首先默认交易价格为当前价格
+                if (res['fills'] && res['fills'].length > 0 && res['fills'][0]['price']) {
+                  tradePrice = res['fills'][0]['price'] // 更新为交易记录中的第一个价格
+                }
+                log(`买入币种为：${symbol}, 买单量为：${quantity}, 买单价格为：${tradePrice}`, flag)
+                Api.notifyBuyOrderSuccess(symbol, quantity, tradePrice) // 发送通知
+  
+                trade.buy_quantity += quantity // 更新已购买数量
+                trade.buy_price = tradePrice // 最后一次买入价格
+                trade.sell_price = round(tradePrice * (1 + trade.rate / 100), 6) // 更新的卖出价格
+                if (trade.sell_price < nowPrice) {
+                  trade.sell_price = round(nowPrice * (1 + trade.rate / 100), 6) // 更新的卖出价格
+                }
+  
+                // 买单记录
+                trade.history_trade.unshift({
+                  symbol,
+                  quantity,
+                  price: tradePrice,
+                  side: BuySide.BUY,
+                  time: dateFormat(),
+                  sell_price: trade.sell_price, // 根据当时的买入价格，设定应该卖出的价格
+                  lowest_sell_price: trade.sell_price, // 最低卖价
+                  low_num: 0, // 容错次数
+                  stop_loss: stop_loss, // 止损率
+                  isSell: false,
+                }) // 头部插入一条，这样容易直接找到最新的记录
+                log(trade, flag)
+              }
+            }
+          }
         }
         return trade
       })
